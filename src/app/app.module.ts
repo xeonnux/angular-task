b9cba41cc269662bb6955b252d5048eee57383e9
Fabrice Madre <stepwiseltd@gmail.com>
2021-09-11 22:25:04 +0400

    create component folder to reorganise components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ButtonComponent } from './button/button.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
